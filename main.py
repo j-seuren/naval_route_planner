51f150e1eed4d8a3925be104f4032e2d61924666
JobS <job.seuren@gmail.com>
2020-05-13 18:45:48 +0200

    Outdated main

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
-import get_data as get
+from preprocess_shapefiles import split_large_polygon
 from random import seed
+import pandas as pd
 import pyvisgraph as vg
 import fiona
 from shapely.geometry import shape, mapping, Point, Polygon, MultiPolygon
-import route
+import solution
 import pickle
-from mutation import insertion
+# from mutation import insertion, init_wp_deviation
 
-seed(1)
 
-from haversine import haversine
+seed(2)
 
 # # Load data
-# seca_areas = get.seca()
-speeds_FM = get.speed_table('Fairmaster')
-# shore_areas = get.shore_areas('crude')
-shorelines_shp_fp = 'C:/dev/data/gshhg-shp-2.3.7/GSHHS_shp/f/GSHHS_f_L1.shp'
-path_shp = ([pt for pt in fiona.open('output/shapefiles/path_shape.shp')])
-with open('output\path_list_l', 'rb') as file:
-    path_list = pickle.load(file)
-    print('Path contains {} points.'.format(len(path_list)))
-
+data_fp = 'C:/dev/data'
+# seca_areas = pd.read_csv(data_fp + '/seca_areas.csv').drop('index', axis=1)
+speeds_FM = pd.read_excel(data_fp + '/speed_table.xlsx', sheet_name='Fairmaster')
 vessel_speed = speeds_FM['Speed'][0]
 fuel_rate = speeds_FM['Fuel'][0]
 print('Vessel speed {} knots.'.format(vessel_speed))
 print('Fuel rate {} tons/day.'.format(fuel_rate))
 
+# Get shorelines shapefile
+res = 'f'
+
+shorelines_shp_fp = 'C:/dev/data/gshhg-shp-2.3.7/GSHHS_shp/' + res + '/GSHHS_' + res + '_L1.shp'
+shoreline_polygons = fiona.open(shorelines_shp_fp)
+
+# Get path shapefile and list, created by visibility_path.py`
+path_shp = ([pt for pt in fiona.open('output/shapefiles/path_shape.shp')])
+with open('output\path_list_c', 'rb') as file:
+    path_list = pickle.load(file)
+    print('Path contains {} points.'.format(len(path_list)))
+
 # Create list of Waypoints
 waypoints = []
 for point in path_list:
-    wp = route.Waypoint(point.x, point.y, )
+    wp = solution.Waypoint(point.x, point.y, )
     waypoints.append(wp)
 
 # Create list of Edges
 edges = []
 v = waypoints[0]
 for w in waypoints[1:]:
-    edge = route.Edge(v, w, vessel_speed)
+    edge = solution.Edge(v, w, vessel_speed)
     v = w
     edges.append(edge)
 
@@ -44,36 +50,38 @@ for w in waypoints[1:]:
 #     print(edge.intersect_polygon(shorelines_shp_fp))
 
 # Create Route object
-route = route.Route(edges)
+route = solution.Route(edges)
 r_travel_time = route.travel_time()
 print('Travel time is {} hours.'.format(round(r_travel_time, 1)))
 r_fuel_consumption = route.fuel(fuel_rate)
 print('Total fuel consumption is {} tons'.format(round(r_fuel_consumption, 1)))
 
-with open('output/route1', 'wb') as file:
-    pickle.dump(route, file)
+# # Check waypoint location feasibility
+# points_in_polygon = []
+# for waypoint in route.waypoints:
+#     polygon = waypoint.in_polygon(shorelines_shp_fp, return_polygon=True)
+#     if polygon:
+#         points_in_polygon.append([waypoint, polygon])
 
-route2 = route
-for i in range(10):
-    route2 = insertion(route2, 1, 0.1, shorelines_shp_fp)
-r_travel_time2 = route2.travel_time()
-print('Travel time is {} hours.'.format(round(r_travel_time2, 1)))
-r_fuel_consumption2 = route2.fuel(fuel_rate)
-print('Total fuel consumption is {} tons'.format(round(r_fuel_consumption2, 1)))
+# Check polygon crossings
+polygon_crossings = []
+count = 0
+for edge in route.edges:
+    count += 1
+    print(count)
+    start_time = time.time()
+    polygon = edge.crosses_polygon(shorelines_shp_fp, return_polygon=True)  # 3.1 - 3.8 seconds
+    print("--- %s seconds ---" % (time.time() - start_time))
+    if polygon:
+        polygon_crossings.append([edge, polygon])
 
-with open('output/route2', 'wb') as file:
-    pickle.dump(route2, file)
+# with open('output/route_c', 'wb') as file:
+#     pickle.dump(route, file)
 
-def pts_shp_in_polygon(points, polygons_shp):  # Check if Fiona list of points lie within polygons shapefile
-    multi_pol = fiona.open(polygons_shp)
-    multi = multi_pol.next()
-    points_in_polygon = []
-    for idx, pt in enumerate(points):
-        pt = shape(pt['geometry'])
-        if pt.within(shape(multi['geometry'])):
-            points_in_polygon.append([idx, shape(points[i]['geometry'])])
-            print('Point {} is in polygon.'.format(idx), shape(points[idx]['geometry']))
-    return points_in_polygon
+
+# with open('output/route2', 'wb') as file:
+#     pickle.dump(route2, file)
+
 
 # Check if points in path lie in polygon
 # for point in waypoints:
