1808990b916f17a229fca8bb50646e42ea7fd9d7
JobS <job.seuren@gmail.com>
2020-05-26 16:39:26 +0200

    Updated plots

---
+++ plot_edge.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import pickle
+import cartopy.crs as ccrs
+from cartopy.io import shapereader
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from shapely.geometry import LineString
+
+
+def plot_edge(edge):
+    def make_map(projection=ccrs.PlateCarree()):
+        fig_f, ax_f = plt.subplots(figsize=(9, 13),
+                               subplot_kw=dict(projection=projection))
+        gl = ax.gridlines(draw_labels=True)
+        gl.xlabels_top = gl.ylabels_right = False
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+        return fig_f, ax_f
+
+    extent = [min(edge.v.x, edge.w.x) - 1, max(edge.v.x, edge.w.x) + 1, min(edge.v.y, edge.w.y) - 1, max(edge.v.y, edge.w.y) + 1]  # [left, right, bottom, top)
+
+    fig, ax = make_map(projection=ccrs.PlateCarree())
+    ax.set_extent(extent)
+
+    polygons = shapereader.Reader('C:/dev/data/gshhg-shp-2.3.7/GSHHS_shp/l/GSHHS_l_L1.shp')
+
+    for record, geometry in zip(polygons.records(), polygons.geometries()):
+        ax.add_geometries([geometry], ccrs.PlateCarree(), facecolor='lightgray', edgecolor='black')
+
+    point1 = [edge.v.y, edge.v.x]
+    point2 = [edge.w.y, edge.w.x]
+
+    plt.plot([point1[1], point2[1]], [point1[0], point2[0]], color='blue', linewidth=1, marker='o', markersize=3, transform=ccrs.PlateCarree())
+
+    edge_line = LineString([(edge.v.x, edge.v.y), (edge.w.x, edge.w.y)])
+    area_radius = 0.5*edge_line.length
+    print(area_radius)
+    area_input = edge_line.centroid.buffer(area_radius)
+
+    x_area, y_area = area_input.exterior.coords.xy
+    ax.plot(x_area, y_area)
+
+    plt.show()
--- plot_route.py
+++ plot_route.py
@@ -3,9 +3,12 @@ import pickle
 
 # Open solution.route file
 directory = 'output/'
-file_name = 'test_route'
+file_name = 'population01'
+output_file_name = 'population_plot_03'
 with open(directory + file_name, 'rb') as openfile:
-    route = pickle.load(openfile)
+    population = pickle.load(openfile)
+
+route = population[-3]
 
 # Plot of the path using folium
 path_points = [[waypoint.y, waypoint.x] for waypoint in route.waypoints]
@@ -19,6 +22,6 @@ folium.Marker(path_points[0], popup=str(route.waypoints[0]), icon=folium.Icon(co
 folium.Marker(path_points[-1], popup=str(route.waypoints[-1]), icon=folium.Icon(color='red')).add_to(geomap)
 
 # Save the interactive plot as a map
-output_name = directory + file_name + '.html'
+output_name = directory + output_file_name + '.html'
 geomap.save(output_name)
 print('Output saved to: {}'.format(output_name))
