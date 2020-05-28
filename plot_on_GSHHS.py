ea2f5b5a78dfa4002c740cfe94c4f0760d053bab
JobS <job.seuren@gmail.com>
2020-05-28 15:10:55 +0200

    Plotting for multiple solutions
    and visibility path

--- plot_on_GSHHS.py
+++ plot_on_GSHHS.py
@@ -9,13 +9,13 @@ def make_map(projection=ccrs.PlateCarree()):
     fig, ax = plt.subplots(figsize=(9, 13),
                            subplot_kw=dict(projection=projection))
     gl = ax.gridlines(draw_labels=True)
-    gl.xlabels_top = gl.ylabels_right = False
+    gl.top_labels = gl.right_labels = False
     gl.xformatter = LONGITUDE_FORMATTER
     gl.yformatter = LATITUDE_FORMATTER
     return fig, ax
 
 
-extent = [-60, 0, 45, 50]  # [left, right, bottom, top)
+extent = [-60, 0, 44, 50]  # [left, right, bottom, top)
 
 fig, ax = make_map(projection=ccrs.PlateCarree())
 ax.set_extent(extent)
@@ -25,13 +25,44 @@ for record, geometry in zip(shp.records(), shp.geometries()):
     ax.add_geometries([geometry], ccrs.PlateCarree(), facecolor='lightgray',
                       edgecolor='black')
 
-# Open solution.route file
-with open('output/route_ocean', 'rb') as file:
-    route = pickle.load(file)
-route_waypoint_list = [[point.y, point.x] for point in route.waypoints]
-prev_point = route_waypoint_list[0]
-for point in route_waypoint_list[1:]:
-    plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]],
-             color='blue', linewidth=1, marker='o', markersize=3, transform=ccrs.PlateCarree())
+# # Open solution.route file
+# with open('output/population01', 'rb') as file:
+#     route = pickle.load(file)
+#
+# route = route[0]
+#
+# route_waypoint_list = [[point.y, point.x] for point in route.waypoints]
+# prev_point = route_waypoint_list[0]
+# for point in route_waypoint_list[1:]:
+#     plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]],
+#              color='blue', linewidth=1, marker='o', markersize=3, transform=ccrs.PlateCarree())
+#     prev_point = point
+
+# Parents plotting
+with open('output/pareto_solutions01', 'rb') as file:
+    parents = pickle.load(file)
+parent = parents[0]
+colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan',
+          'burlywood', 'lime', 'lightsteelblue', 'slateblue', 'gainsboro', 'dimgray', 'lightsalmon']
+for c, parent in enumerate(parents[:10]):
+    parent_waypoint_list = [[point.y, point.x] for point in parent.waypoints]
+    prev_point = parent_waypoint_list[0]
+
+    for point in parent_waypoint_list[1:]:
+        plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]], color=colors[c], linewidth=1, marker='o',
+                 markersize=3, transform=ccrs.PlateCarree())
+        prev_point = point
+
+
+# Plot visibility route
+with open('output/visibility_route', 'rb') as file:
+    visibility_route = pickle.load(file)
+vis_route_wps = [[point.y, point.x] for point in visibility_route.waypoints]
+prev_point = vis_route_wps[0]
+
+for point in vis_route_wps[1:]:
+    plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]], linewidth=1, marker='o',
+             markersize=3, transform=ccrs.PlateCarree())
     prev_point = point
+
 plt.show()
