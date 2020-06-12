6e982ccf7e963f0ba0609caaee1931f56c3e8fb9
JobS <job.seuren@gmail.com>
2020-06-12 15:39:10 +0200

    Made function of plot_on_GSHHS

--- plot/plot_on_GSHHS.py
+++ plot/plot_on_GSHHS.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import pickle
 import cartopy.crs as ccrs
+from operator import attrgetter
 from cartopy.io import shapereader
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from math import floor, ceil
 
 
 def make_map(projection=ccrs.PlateCarree()):
@@ -15,54 +17,30 @@ def make_map(projection=ccrs.PlateCarree()):
     return fig, ax
 
 
-extent = [-60, 0, 44, 50]  # [left, right, bottom, top)
+def plot_on_gshhs(route):
+    min_x, min_y = min(route.waypoints, key=attrgetter('lon')), min(route.waypoints, key=attrgetter('lat'))
+    max_x, max_y = max(route.waypoints, key=attrgetter('lon')), max(route.waypoints, key=attrgetter('lat'))
+    m = 5  # margin
+    extent = [max(floor(min_x.lon) - m, -180), min(ceil(max_x.lon) + m, 180),
+              max(floor(min_y.lat) - m, -90), min(ceil(max_y.lat) + m, 90)]  # [left, right, bottom, top)
 
-fig, ax = make_map(projection=ccrs.PlateCarree())
-ax.set_extent(extent)
+    fig, ax = make_map(projection=ccrs.PlateCarree())
+    ax.set_extent(extent, crs=ccrs.PlateCarree())
 
-shp = shapereader.Reader('C:/dev/data/gshhg-shp-2.3.7/GSHHS_shp/l/GSHHS_l_L1.shp')
-for record, geometry in zip(shp.records(), shp.geometries()):
-    ax.add_geometries([geometry], ccrs.PlateCarree(), facecolor='lightgray',
-                      edgecolor='black')
+    # Plot shorelines
+    shp = shapereader.Reader('C:/dev/data/gshhg-shp-2.3.7/GSHHS_shp/l/GSHHS_l_L1.shp')
+    for record, geometry in zip(shp.records(), shp.geometries()):
+        ax.add_geometries([geometry], ccrs.PlateCarree(), facecolor='lightgray', edgecolor='black')
 
-# # Open solution.route file
-# with open('output/population01', 'rb') as file:
-#     route = pickle.load(file)
-#
-# route = route[0]
-#
-# route_waypoint_list = [[point.y, point.x] for point in route.waypoints]
-# prev_point = route_waypoint_list[0]
-# for point in route_waypoint_list[1:]:
-#     plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]],
-#              color='blue', linewidth=1, marker='o', markersize=3, transform=ccrs.PlateCarree())
-#     prev_point = point
-
-# Parents plotting
-with open('../output/pareto_solutions01', 'rb') as file:
-    parents = pickle.load(file)
-parent = parents[0]
-colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan',
-          'burlywood', 'lime', 'lightsteelblue', 'slateblue', 'gainsboro', 'dimgray', 'lightsalmon']
-for c, parent in enumerate(parents[:10]):
-    parent_waypoint_list = [[point.y, point.x] for point in parent.waypoints]
-    prev_point = parent_waypoint_list[0]
-
-    for point in parent_waypoint_list[1:]:
-        plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]], color=colors[c], linewidth=1, marker='o',
-                 markersize=3, transform=ccrs.PlateCarree())
-        prev_point = point
+    # Plot waypoints
+    waypoints = [[point.lon, point.lat] for point in route.waypoints]
+    edges = zip(waypoints[:-1], waypoints[1:])
+    for e in edges:
+        plt.plot([e[0][0], e[1][0]], [e[0][1], e[1][1]],
+                 color='blue', linewidth=1, marker='o', markersize=3, transform=ccrs.PlateCarree())
 
 
-# Plot visibility route
-with open('../output/visibility_route', 'rb') as file:
-    visibility_route = pickle.load(file)
-vis_route_wps = [[point.y, point.x] for point in visibility_route.waypoints]
-prev_point = vis_route_wps[0]
-
-for point in vis_route_wps[1:]:
-    plt.plot([prev_point[1], point[1]], [prev_point[0], point[0]], linewidth=1, marker='o',
-             markersize=3, transform=ccrs.PlateCarree())
-    prev_point = point
-
-plt.show()
+# with open('C:/dev/projects/naval_route_planner/output/graph_route', 'rb') as f:
+#     pareto_solutions = pickle.load(f)
+# plot_on_gshhs(pareto_solutions)
+# plt.show()
