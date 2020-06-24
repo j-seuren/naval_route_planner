fbc85eb96a22f67cc65a3aa69a7bbddea5f2805b
JobS <job.seuren@gmail.com>
2020-06-24 10:04:01 +0200

    Included best solution evaluation
    And plotting different paths

--- plot_on_GSHHS.py
+++ plot_on_GSHHS.py
@@ -5,9 +5,10 @@ import pandas as pd
 import pickle
 from math import floor, ceil
 import numpy as np
+import ocean_current
 
 
-def draw_basemap(individuals, vessel):
+def draw_basemap(individuals, vessel, cb=True, plot_vectors=False, uin=None, vin=None, lons=None, lats=None):
     wps = []
     for ind in individuals:
         wps.extend([item[0] for item in ind])
@@ -23,20 +24,29 @@ def draw_basemap(individuals, vessel):
     m.drawmeridians(np.arange(-180., 180., 10.), labels=[0, 0, 0, 1], fontsize=10)
     m.drawcoastlines()
     m.fillcontinents()
-    # m.bluemarble()
 
-    # Create colorbar
+    if plot_vectors:
+        # Transform vector and coordinate data
+        vec_lon = int(uin.shape[1] / (10 * 360 / (right - left)))
+        vec_lat = int(uin.shape[0] / (10 * 180 / (top - bottom)))
+        u_rot, v_rot, x, y = m.transform_vector(uin, vin, lons, lats, vec_lon, vec_lat, returnxy=True)
+
+        vec_plot = m.quiver(x, y, u_rot, v_rot, scale=50)
+        plt.quiverkey(vec_plot, 0.2, -0.2, 1, '1 knot', labelpos='W')  # Position and reference label
+
     col_map = cm.jet
-    sm = plt.cm.ScalarMappable(cmap=col_map)
-    col_bar = plt.colorbar(sm, norm=plt.Normalize(vmin=min(vessel.speeds), vmax=max(vessel.speeds)))
-    max_s = max(vessel.speeds)
-    min_s = min(vessel.speeds)
-    col_bar.ax.set_yticklabels(['{}'.format(min_s),
-                                '{}'.format(round((1 / 5) * (max_s - min_s) + min_s, 1)),
-                                '{}'.format(round((2 / 5) * (max_s - min_s) + min_s, 1)),
-                                '{}'.format(round((3 / 5) * (max_s - min_s) + min_s, 1)),
-                                '{}'.format(round((4 / 5) * (max_s - min_s) + min_s, 1)),
-                                '{}'.format(round(max_s, 1))])
+    if cb:
+        # Create colorbar
+        sm = plt.cm.ScalarMappable(cmap=col_map)
+        col_bar = plt.colorbar(sm, norm=plt.Normalize(vmin=min(vessel.speeds), vmax=max(vessel.speeds)))
+        max_s = max(vessel.speeds)
+        min_s = min(vessel.speeds)
+        col_bar.ax.set_yticklabels(['{}'.format(min_s),
+                                    '{}'.format(round((1 / 5) * (max_s - min_s) + min_s, 1)),
+                                    '{}'.format(round((2 / 5) * (max_s - min_s) + min_s, 1)),
+                                    '{}'.format(round((3 / 5) * (max_s - min_s) + min_s, 1)),
+                                    '{}'.format(round((4 / 5) * (max_s - min_s) + min_s, 1)),
+                                    '{}'.format(round(max_s, 1))])
 
     for ind in individuals:
         # Create colors
@@ -79,17 +89,28 @@ if __name__ == "__main__":
 
     populations = []
     individuals = []
-    idx = 0
-    for i in range(2):
-        with open('C:/dev/projects/naval_route_planner/output/path_0/ 22_13_27_population_sub_path_{}'.format(i), 'rb') as f:
+    for i in range(1):
+        with open('output/path_1/10_00_24_population_sub_path_{}'.format(i), 'rb') as f:
             population = pickle.load(f)
-        individual = population[idx]
+
+        min_tt, min_fc = 9999999999, 99999999999
+        tt_ind, fc_ind = None, None
+        for ind in population:
+            travel_time, fuel_consumption = ind.fitness.values
+            if travel_time < min_tt:
+                tt_ind = ind
+                min_tt = travel_time
+            if fuel_consumption < min_fc:
+                fc_ind = ind
+                min_fc = fuel_consumption
+
+        individuals.append(fc_ind)
+        individuals.append(tt_ind)
+        print('Fuel individual:', fc_ind.fitness.values)
+        print('Travel time individual: ', tt_ind.fitness.values)
 
         populations.append(population)
-        individuals.append(individual)
+    u_out, v_out, lons, lats = ocean_current.read_netcdf('20160101')
+    draw_basemap(individuals, vessel, cb=False, plot_vectors=True, uin=u_out, vin=v_out, lons=lons, lats=lats)
 
-        print('Speeds: ', [item[1] for item in individual])
-        print(individual.fitness.values)
-    draw_basemap(individuals, vessel)
-    # draw_basemap([[[(-5.352121, 48.021295), 10], [(-72.984139, 40.235248), None]]], vessel)
     plt.show()
