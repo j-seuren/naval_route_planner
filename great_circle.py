dd168016d2b137febeecfcb9d8c869167668a169
JobS <job.seuren@gmail.com>
2020-07-02 23:09:00 +0200

    Moved nautical mile conversion

--- great_circle.py
+++ great_circle.py
@@ -1,15 +1,15 @@
-def distance(lon1, lat1, lon2, lat2, gc):
+def distance(lon1, lat1, lon2, lat2, geod):
     """
         Get great circle distance from the longitude-latitude
         pair ``lon1,lat1`` to ``lon2,lat2``
         Returns great circle distance
         """
-    az12, az21, dist = gc.inv(lon1, lat1, lon2, lat2)
+    az12, az21, dist = geod.inv(lon1, lat1, lon2, lat2)
 
-    return dist * 0.000539957
+    return dist
 
 
-def points(lon1, lat1, lon2, lat2, dist, gc, del_s):
+def points(lon1, lat1, lon2, lat2, dist, geod, del_s):
     """
     Get great circle points from the longitude-latitude
     pair ``lon1,lat1`` to ``lon2,lat2``
@@ -23,7 +23,7 @@ def points(lon1, lat1, lon2, lat2, dist, gc, del_s):
     Returns two lists of lons, lats points on great circle
     """
     n_points = int((dist + 0.5 * del_s) / del_s)
-    lon_lats = gc.npts(lon1, lat1, lon2, lat2, n_points)
+    lon_lats = geod.npts(lon1, lat1, lon2, lat2, n_points)
     lons, lats = [lon1], [lat1]
     for lon, lat in lon_lats:
         lons.append(lon)
