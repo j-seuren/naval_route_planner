e5e6e8ac59d75da434a26e879b3af65943d3adf3
JobS <job.seuren@gmail.com>
2020-07-08 14:25:23 +0200

    Updated caching
    and minor fixes

--- evaluation.py
+++ evaluation.py
@@ -1,39 +1,65 @@
-import great_circle
-import ocean_current
+import collections
+import functools
+import numpy as np
 import operator
 import pickle
 import rtree
 
-from datetime import datetime, timedelta
-from math import copysign, sqrt, isnan
+from datetime import timedelta
+from math import copysign, sqrt
 from shapely.geometry import LineString
+
+
+class Memoized(object):
+    """Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable: better to not cache than blow up.
+            print(args, 'not hashable')
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        """Return the function's docstring."""
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        return functools.partial(self.__call__, obj)
 
 
 class Evaluator:
     def __init__(self,
                  vessel,
-                 prepared_polys,
+                 prep_polys,
                  rtree_idx,
-                 geod,
                  seca_factor,
-                 start_date=datetime(2016, 1, 1),
-                 del_s=100,
-                 del_sc=15,
-                 include_currents=True):
-        self.vessel = vessel
-        self.prepared_polys = prepared_polys
-        self.rtree_idx = rtree_idx
-        self.geod = geod
-        self.seca_factor = seca_factor
-        self.start_date = start_date                                    # Start date for initializing ocean currents
-        self.del_s = del_s                                              # Maximum segment length (nautical miles)
-        self.del_sc = del_sc                                            # Maximum segment length for current calculation
-        self.include_currents = include_currents
-        self.dist_cache = dict()
-        self.feas_cache = dict()
-        self.feas_points_cache = dict()
-        self.curr_points_cache = dict()
-        self.travel_time_cache = dict()
+                 start_date,
+                 gc,
+                 incl_curr=True,
+                 del_sf=100,
+                 del_sc=15, ):
+        self.vessel = vessel            # Vessel class instance
+        self.prep_polys = prep_polys    # Prepared land polygons
+        self.rtree_idx = rtree_idx      # prep_polys' R-tree spatial index
+        self.seca_factor = seca_factor  # Multiplication factor for SECA fuel
+        self.start_date = start_date    # Start date of voyage
+        self.del_sf = del_sf            # Max segment length (for feasibility)
+        self.del_sc = del_sc            # Max segment length (for currents)
+        self.incl_curr = incl_curr      # Boolean for including currents
+        self.gc = gc                    # Geod class instance
 
         with open('C:/dev/data/seca_areas_csv', 'rb') as f:
             self.secas = pickle.load(f)
@@ -41,116 +67,82 @@ class Evaluator:
         self.rtree_idx_seca = rtree.index.Index()
         for idx, seca in enumerate(self.secas):
             self.rtree_idx_seca.insert(idx, seca.bounds)
+        self.current_data = None
 
-        # Initialize "CurrentData"
-        self.current_data = ocean_current.CurrentData()
-
-    def evaluate(self, individual):
+    def evaluate(self, ind):
         # Initialize variables
-        travel_time = fuel_consumption = 0.0
+        tt = fc = 0.0
 
-        for e in range(len(individual) - 1):
-            p1, p2, boat_speed = individual[e][0], individual[e + 1][0], individual[e][1]
-            k = tuple(sorted([p1, p2]))
+        for e in range(len(ind) - 1):
+            # Get edge waypoints and edge boat speed
+            p1, p2 = sorted((ind[e][0], ind[e + 1][0]))
+            boat_speed = ind[e][1]
 
-            e_dist = self.dist_cache.get(k, False)
-            if not e_dist:  # Never steps in IF-statement
-                print('computes distance')
-                e_dist = great_circle.distance(p1[0], p1[1], p2[0], p2[1], self.geod)
-                self.dist_cache[k] = e_dist
+            # Compute travel time over edge
+            e_tt = self.e_tt(p1, p2, tt, boat_speed)
 
-            if self.include_currents:
-                k3 = k + (travel_time, boat_speed)
-                e_travel_time = self.travel_time_cache.get(k3, False)
-                if not e_travel_time:
-                    # Split edge in segments (seg) of max seg_length in km
-                    points = self.curr_points_cache.get(k, False)
-                    if not points:
-                        points = great_circle.points(p1[0], p1[1], p2[0], p2[1], e_dist, self.geod, self.del_sc)
-                        self.curr_points_cache[k] = points
-                    lons, lats = points[0], points[1]
-                    e_travel_time = 0.0
-                    for i in range(len(lons) - 1):
-                        p1, p2 = (lons[i], lats[i]), (lons[i+1], lats[i+1])
-                        seg_dist = great_circle.distance(p1[0], p1[1], p2[0], p2[1], self.geod)
-                        now = self.start_date + timedelta(hours=travel_time + e_travel_time)
-                        seg_travel_time = self.get_seg_travel_time(p1, p2, boat_speed, seg_dist, now)
-                        e_travel_time += seg_travel_time
-                    self.travel_time_cache[k3] = e_travel_time
-            else:
-                e_travel_time = e_dist / boat_speed
+            # Compute fuel consumption over edge
+            e_fc = self.vessel.fuel_rates[boat_speed] * e_tt  # Tons
 
             # If edge intersects SECA increase fuel consumption by seca_factor
             if edge_x_geos(p1, p2, self.rtree_idx_seca, self.secas):
-                seca_factor = self.seca_factor
-            else:
-                seca_factor = 1
-            edge_fuel_consumption = self.vessel.fuel_rates[boat_speed] * e_travel_time * seca_factor  # Tons
+                e_fc *= self.seca_factor
 
             # Increment objective values
-            travel_time += e_travel_time
-            fuel_consumption += edge_fuel_consumption
+            tt += e_tt
+            fc += e_fc
 
-        return travel_time, fuel_consumption
+        return tt, fc
 
-    def feasible(self, individual):
-        for i in range(len(individual) - 1):
-            p1, p2 = individual[i][0], individual[i + 1][0]
-            if not self.edge_feasible(p1, p2):
+    def feasible(self, ind):
+        for i in range(len(ind)-1):
+            p1, p2 = sorted((ind[i][0], ind[i+1][0]))
+            if not self.e_feasible(p1, p2):
                 return False
         return True
 
-    def edge_feasible(self, p1, p2):
-        # First check if feasibility check is already performed
-        k = tuple(sorted([p1, p2]))
-        feasible = self.feas_cache.get(k, None)
-        if feasible == 1:
-            return True
-        elif feasible == 0:
-            return False
-
-        dist = self.dist_cache.get(k, False)
-        if not dist:
-            dist = great_circle.distance(p1[0], p1[1], p2[0], p2[1], self.geod)
-            self.dist_cache[k] = dist
-
-        points = self.feas_points_cache.get(k, False)
-        if not points:
-            points = great_circle.points(p1[0], p1[1], p2[0], p2[1], dist, self.geod, self.del_s)
-            self.feas_points_cache[k] = points
-        lons, lats = points[0], points[1]
+    @Memoized
+    def e_feasible(self, p1, p2):
+        dist = self.gc.distance(p1, p2)
+        lons, lats = self.gc.points(p1, p2, dist, self.del_sf)
         for i in range(len(lons) - 1):
             # Compute line bounds
             q1, q2 = (lons[i], lats[i]), (lons[i + 1], lats[i + 1])
-
-            if edge_x_geos(q1, q2, self.rtree_idx, self.prepared_polys):
-                self.feas_cache[k] = 0
+            if edge_x_geos(q1, q2, self.rtree_idx, self.prep_polys):
                 return False
-        self.feas_cache[k] = 1
         return True
 
-    def get_seg_travel_time(self, p1, p2, boat_speed, distance, date_time):
-        # Middle point of edge
+    def e_tt(self, p1, p2, tt, boat_speed):
+        dist = self.gc.distance(p1, p2)
+        if self.incl_curr:
+            # Split edge in segments (seg) of del_sc in nautical miles
+            lons, lats = self.gc.points(p1, p2, dist, self.del_sc)
+            e_tt = 0.0
+            for i in range(len(lons) - 1):
+                q1, q2 = sorted(((lons[i], lats[i]), (lons[i+1], lats[i+1])))
+                tot_hours = tt + e_tt
+                seg_tt = self.seg_tt(q1, q2, boat_speed, tot_hours)
+                e_tt += seg_tt
+            return e_tt
+        else:
+            return dist / boat_speed
+
+    def seg_tt(self, p1, p2, boat_speed, delta_hours):
+        now = self.start_date + timedelta(hours=delta_hours)
+        dist = self.gc.distance(p1, p2)
+
+        # Get current of nearest grid point to middle point of edge
         x_m, y_m = (item / 2 for item in map(operator.add, p1, p2))
-
-        u, v, _, _ = self.current_data.get_currents(date_time)
-
-        # Get coordinates of nearest grid point
-        lon_idx, lat_idx = int(round((x_m + 179.875) / 0.25)), int(round((y_m + 89.875) / 0.25))
-
-        u_m = float(u[lat_idx, lon_idx].compute())
-        v_m = float(v[lat_idx, lon_idx].compute())
-
-        # If u, v value is nan, set ocean current to 0
-        if isnan(u_m) or isnan(v_m):
-            u_m = v_m = 0
+        lon_idx = int(round((x_m + 179.875) / 0.25))
+        lat_idx = int(round((y_m + 89.875) / 0.25))
+        u_m, v_m = self.current_data.get_grid_pt_current(now, lon_idx, lat_idx)
 
         # Calculate speed over ground
-        SOG = speed_over_ground(p1, p2, u_m, v_m, boat_speed)
-        return distance / SOG
+        sog = calc_sog(p1, p2, u_m, v_m, boat_speed)
+        return dist / sog
 
 
-def speed_over_ground(p, q, c_u, c_v, boat_speed):
+def calc_sog(p1, p2, c_u, c_v, boat_speed):
     """
     Determine speed over ground (SOG) between points P and Q:
     SOG = boat speed + current speed (vectors)
@@ -160,8 +152,7 @@ def speed_over_ground(p, q, c_u, c_v, boat_speed):
     """
 
     # Get equation for line PQ
-    dx = q[0] - p[0]
-    dy = q[1] - p[1]
+    dx, dy = np.subtract(p2, p1)
     try:
         alpha = dy / (dx + 0.0)
     except ZeroDivisionError:
@@ -175,47 +166,46 @@ def speed_over_ground(p, q, c_u, c_v, boat_speed):
     c = c_u ** 2 + c_v ** 2 - boat_speed ** 2
     d = b ** 2 - 4 * a * c  # discriminant
 
-    assert d >= 0, "There exist no real solutions between points {} and {}".format(p, q)
+    assert d >= 0, "No real solutions between pts {} and {}".format(p1, p2)
 
     if d == 0:
         x = (-b + sqrt(d)) / (2 * a)
         y = alpha * x
-        SOG = sqrt(x ** 2 + y ** 2)
+        sog = sqrt(x ** 2 + y ** 2)
     else:
-        rt = sqrt(d)
-        root1 = (-b + rt) / (2 * a)
-        root2 = (-b - rt) / (2 * a)
-        if copysign(1, root1) == copysign(1, dx) and copysign(1, root2) == copysign(1, dx):
+        sqrt_d = sqrt(d)
+        r1 = (-b + sqrt_d) / (2 * a)
+        r2 = (-b - sqrt_d) / (2 * a)
+        if copysign(1, r1) == copysign(1, dx) \
+                and copysign(1, r2) == copysign(1, dx):
             # If both roots return resultant vector in right direction,
             # use resultant vector with greatest length
-            y1, y2 = alpha * root1, alpha * root2
-            v1, v2 = sqrt(root1 ** 2 + y1 ** 2), sqrt(root2 ** 2 + y2 ** 2)
+            y1, y2 = alpha * r1, alpha * r2
+            v1, v2 = sqrt(r1 ** 2 + y1 ** 2), sqrt(r2 ** 2 + y2 ** 2)
             if v1 > v2:
-                SOG = v1
+                sog = v1
             else:
-                SOG = v2
+                sog = v2
         else:
-            if copysign(1, root2) == copysign(1, dx):
-                x = root2
+            if copysign(1, r2) == copysign(1, dx):
+                x = r2
             else:
-                x = root1
+                x = r1
             y = alpha * x
-            SOG = sqrt(x ** 2 + y ** 2)
+            sog = sqrt(x ** 2 + y ** 2)
 
-    return SOG
+    return sog
 
 
 def edge_x_geos(p1, p2, rtree_idx, geos):
-    line_bounds = (min(p1[0], p2[0]), min(p1[1], p2[1]), max(p1[0], p2[0]), max(p1[1], p2[1]))
-
-    # Returns the geometry indices of the minimum bounding rectangles of polygons that intersect the edge bounds
-    mbr_intersections = list(rtree_idx.intersection(line_bounds))
+    line = LineString([p1, p2])
+    # Returns the geometry indices of the minimum bounding rectangles
+    # of polygons that intersect the edge bounds
+    mbr_intersections = list(rtree_idx.intersection(line.bounds))
     if mbr_intersections:
-        # Create LineString if there is at least one minimum bounding rectangle intersection
-        line_string = LineString([(p1[0], p1[1]), (p2[0], p2[1])])
-
-        # For every mbr intersection check if its polygon is actually intersect by the edge
+        # For every mbr intersection
+        # check if its polygon is actually intersect by the edge
         for idx in mbr_intersections:
-            if geos[idx].intersects(line_string):
+            if geos[idx].intersects(line):
                 return True
     return False
