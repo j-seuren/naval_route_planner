a823a9d838b9600985af3e1bd2171c5c2576cdc4
JobS <job.seuren@gmail.com>
2020-07-10 19:20:26 +0200

    Included Hall of Fame
    Maybe switch to Pareto Front

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ class Vessel:
 Nbar = 50
 # GEN = 250
 # N = 100
-N = 200
-GEN = 2000
+N = 20
+GEN = 20
 CXPB = 0.9
 MUTPB = 0.9
 
@@ -42,8 +42,8 @@ class RoutePlanner:
     def __init__(self,
                  start=None,
                  end=None,
-                 start_date=datetime(2016, 1, 1),
-                 seca_factor=1.2,
+                 start_date=datetime(2016, 1, 2),
+                 eca_f=1.2,
                  res='c',
                  spl_th=4,
                  vessel_name='Fairmaster',
@@ -58,7 +58,7 @@ class RoutePlanner:
         self.start = start                    # Start point
         self.end = end                        # End point
         self.start_date = start_date          # Start date of voyage
-        self.seca_factor = seca_factor        # Multiplication factor SECA fuel
+        self.eca_f = eca_f                  # Multiplication factor ECA fuel
         self.vessel = Vessel(vessel_name)     # Vessel class instance
         self.res = res                        # Resolution of shorelines
         self.spl_th = spl_th                  # Threshold for split_polygon
@@ -83,10 +83,25 @@ class RoutePlanner:
         for idx, poly in enumerate(spl_polys):
             self.rtree_idx.insert(idx, poly.bounds)
 
+        # Load eca polygons
+        with open('C:/dev/data/seca_areas_csv', 'rb') as f:
+            self.ecas = pickle.load(f)
+
+        # Populate R-tree index with bounds of ECA polygons
+        self.rtree_idx_eca = rtree.index.Index()
+        for idx, eca in enumerate(self.ecas):
+            self.rtree_idx_eca.insert(idx, eca.bounds)
+
         # Initialize "Evaluator" and register it's functions
-        self.evaluator = evaluation.Evaluator(self.vessel, self.prep_polys,
-                                              self.rtree_idx, seca_factor,
-                                              start_date, self.gc, incl_curr)
+        self.evaluator = evaluation.Evaluator(self.vessel,
+                                              self.prep_polys,
+                                              self.rtree_idx,
+                                              self.ecas,
+                                              self.rtree_idx_eca,
+                                              eca_f,
+                                              start_date,
+                                              self.gc,
+                                              incl_curr)
         self.tb.register("e_feasible", self.evaluator.e_feasible)
         self.tb.register("feasible", self.evaluator.feasible)
         self.tb.register("evaluate", self.evaluator.evaluate)
@@ -99,6 +114,8 @@ class RoutePlanner:
                                                           self.vessel, res,
                                                           self.prep_polys,
                                                           self.rtree_idx,
+                                                          self.ecas,
+                                                          self.rtree_idx_eca,
                                                           self.tb,
                                                           self.gc)
             self.tb.register("get_shortest_paths",
@@ -112,50 +129,57 @@ class RoutePlanner:
         self.tb.register("mutate", self.operators.mutate)
         self.tb.register("mate", self.operators.cx_one_point)
 
-        self.tb.register("population", tools.initRepeat, list)
+        self.tb.register("population", initialization.init_repeat_list)
+
+        # Initialize Statistics
+        stats_fit = tools.Statistics(lambda _ind: _ind.fitness.values)
+        stats_size = tools.Statistics(key=len)
+        self.mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+        self.mstats.register("avg", np.mean, axis=0)
+        self.mstats.register("std", np.std, axis=0)
+        self.mstats.register("min", np.min, axis=0)
+        self.mstats.register("max", np.max, axis=0)
+
+        # Initialize Logbook
+        self.log = tools.Logbook()
+        self.log.header = "gen", "evals", "fitness", "size"
+        self.log.chapters["fitness"].header = "min", "avg", "max"
+        self.log.chapters["size"].header = "min", "avg", "max"
+
+        # Initialize Hall of Fame
+        self.hof = tools.HallOfFame(N)
 
     def spea2(self, seed=None):
+        random.seed(seed)
+
         # Register SPEA2 selection function
         self.tb.register("select", tools.selSPEA2)
 
-        random.seed(seed)
-
-        stats_fit = tools.Statistics(lambda _ind: _ind.fitness.values)
-        stats_size = tools.Statistics(key=len)
-        mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
-        mstats.register("avg", np.mean, axis=0)
-        mstats.register("std", np.std, axis=0)
-        mstats.register("min", np.min, axis=0)
-        mstats.register("max", np.max, axis=0)
-
-        print('Get shortest paths on graph... ', end='')
-        shortest_paths = self.tb.get_shortest_paths()
+        print('Get shortest paths on graph:', end='\n ')
+        sps_dict = self.tb.get_shortest_paths()
         print('done')
 
-        paths, path_logs = {}, {}
-        for p_idx, shortest_path in enumerate(shortest_paths):
-            print('Path {0}/{1}'.format(p_idx+1, len(shortest_paths)))
-            sub_paths, sub_path_logs = {}, {}
-            for sp_idx, sub_sp in enumerate(shortest_path):
-                print('Sub path {0}/{1}'.format(sp_idx+1, len(shortest_path)))
-
-                self.tb.register("individual", initialization.init_individual,
-                                 self.tb, sub_sp)
-
-                log = tools.Logbook()
-                log.header = "gen", "evals", "fitness", "size"
-                log.chapters["fitness"].header = "min", "avg", "max"
-                log.chapters["size"].header = "min", "avg", "max"
+        paths, path_logs, path_hofs = {}, {}, {}
+        for sp_key, sp_dict in sps_dict.items():
+            print('Path {0}/{1}'.format(sp_key+1, len(sps_dict)))
+            sub_paths, sub_path_logs, sub_path_hofs = {}, {}, {}
+            for sub_sp_key, sub_sp_dict in sp_dict.items():
+                print('Sub path {0}/{1}'.format(sub_sp_key+1, len(sp_dict)))
+                self.tb.register("individual", initialization.init_individual, self.tb, sub_sp_dict)
+                self.hof.clear()
+                self.log.clear()
 
                 # Step 1: Initialization
-                print('Initializing population from shortest path... ', end='')
-                pop = self.tb.population(self.tb.individual, N)
+                print('Initializing population from shortest path:', end=' ')
+                pop = self.tb.population(self.tb.individual,
+                                         int(N / len(sub_sp_dict.values()))
+                                         )
                 archive = []
                 curr_gen = 1
                 print('done')
 
                 if self.incl_curr:
-                    self.evaluator.current_data = ocean_current.CurrentData(
+                    self.evaluator.current_data = ocean_current.CurrentOperator(
                         self.start_date, self.get_n_days(pop))
 
                 # Step 2: Fitness assignment
@@ -168,18 +192,20 @@ class RoutePlanner:
                 while True:
                     # Step 3: Environmental selection
                     archive = self.tb.select(pop + archive, k=Nbar)
+                    self.hof.update(archive)
 
                     # Record statistics
-                    record = mstats.compile(archive)
-                    log.record(gen=curr_gen,
-                               evals=(len(inv_inds1) + len(inv_inds2)),
-                               **record)
-                    print(log.stream)
+                    record = self.mstats.compile(archive)
+                    self.log.record(gen=curr_gen,
+                                    evals=(len(inv_inds1) + len(inv_inds2)),
+                                    **record)
+                    print('\r', self.log.stream)
 
                     # Step 4: Termination
                     if curr_gen >= GEN:
-                        sub_paths[sp_idx] = archive
-                        sub_path_logs[sp_idx] = log
+                        sub_paths[sub_sp_key] = archive
+                        sub_path_logs[sub_sp_key] = self.log
+                        sub_path_hofs[sub_sp_key] = self.hof
                         self.tb.unregister("individual")
                         break
 
@@ -205,56 +231,45 @@ class RoutePlanner:
 
                     curr_gen += 1
 
-            paths[p_idx] = sub_paths
-            path_logs[p_idx] = sub_path_logs
-        return paths, path_logs, shortest_paths
+            paths[sp_key] = sub_paths
+            path_logs[sp_key] = sub_path_logs
+            path_hofs[sp_key] = sub_path_hofs
+        return paths, path_logs, path_hofs, sps_dict
 
     def nsga2(self, seed=None):
+        random.seed(seed)
+
         # Register NSGA2 selection function
         self.tb.register("select", tools.selNSGA2)
 
-        random.seed(seed)
-
-        stats_fit = tools.Statistics(lambda _ind: _ind.fitness.values)
-        stats_size = tools.Statistics(key=len)
-        mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
-        mstats.register("avg", np.mean, axis=0)
-        mstats.register("std", np.std, axis=0)
-        mstats.register("min", np.min, axis=0)
-        mstats.register("max", np.max, axis=0)
-
-        print('Get shortest paths on graph... ', end='')
-        shortest_paths = self.tb.get_shortest_paths()
+        print('Get shortest paths on graph:', end='\n ')
+        sps_dict = self.tb.get_shortest_paths()
         print('done')
 
-        paths, path_logs = {}, {}
-        for p_idx, shortest_path in enumerate(shortest_paths):
-            print('Path {0}/{1}'.format(p_idx + 1, len(shortest_paths)))
-            sub_paths, sub_path_logs = {}, {}
-            for sp_idx, sub_sp in enumerate(shortest_path):
-                print('Sub path {0}/{1}'.format(sp_idx + 1, len(shortest_path)))
-
-                self.tb.register("individual", initialization.init_individual,
-                                 self.tb, sub_sp)
-
-                log = tools.Logbook()
-                log.header = "gen", "evals", "fitness", "size"
-                log.chapters["fitness"].header = "min", "avg", "max"
-                log.chapters["size"].header = "min", "avg", "max"
+        paths, path_logs, path_hofs = {}, {}, {}
+        for sp_key, sp_dict in sps_dict.items():
+            print('Path {0}/{1}'.format(sp_key + 1, len(sps_dict)))
+            sub_paths, sub_path_logs, sub_path_hofs = {}, {}, {}
+            for sub_sp_key, sub_sp_dict in sp_dict.items():
+                print('Sub path {0}/{1}'.format(sub_sp_key+1, len(sp_dict)))
+                self.tb.register("individual", initialization.init_individual, self.tb, sub_sp_dict)
+                self.hof.clear()
+                self.log.clear()
 
                 # Step 1: Initialization
-                print('Initializing population from shortest path... ', end='')
-                pop = self.tb.population(self.tb.individual, N)
+                print('Initializing population from shortest path:', end='\n ')
+                pop = self.tb.population(self.tb.individual,
+                                         int(N / len(sub_sp_dict.values())))
                 offspring = []
                 curr_gen = 1
                 print('done')
 
                 if self.incl_curr:
-                    self.evaluator.current_data = ocean_current.CurrentData(
+                    self.evaluator.current_data = ocean_current.CurrentOperator(
                         self.start_date, self.get_n_days(pop))
 
                 # Step 2: Fitness assignment
-                print('Fitness assignment... ', end='')
+                print('Fitness assignment:', end=' ')
                 fits = self.tb.map(self.tb.evaluate, pop)
                 for ind, fit in zip(pop, fits):
                     ind.fitness.values = fit
@@ -262,18 +277,20 @@ class RoutePlanner:
 
                 # Begin the generational process
                 while True:
-                    # Step 3: Environmental selection
+                    # Step 3: Environmental selection (and update HoF)
                     pop = self.tb.select(pop + offspring, N)
+                    self.hof.update(pop)
 
                     # Record statistics
-                    record = mstats.compile(pop)
-                    log.record(gen=curr_gen, evals=len(pop), **record)
-                    print(log.stream)
+                    record = self.mstats.compile(pop)
+                    self.log.record(gen=curr_gen, evals=len(pop), **record)
+                    print('\r', self.log.stream)
 
                     # Step 4: Termination
                     if curr_gen >= GEN:
-                        sub_paths[sp_idx] = pop
-                        sub_path_logs[sp_idx] = log
+                        sub_paths[sub_sp_key] = pop
+                        sub_path_logs[sub_sp_key] = self.log
+                        sub_path_hofs[sub_sp_key] = self.hof
                         self.tb.unregister("individual")
                         break
 
@@ -287,17 +304,19 @@ class RoutePlanner:
                         ind.fitness.values = fit
 
                     curr_gen += 1
-            paths[p_idx] = sub_paths
-            path_logs[p_idx] = sub_path_logs
+            paths[sp_key] = sub_paths
+            path_logs[sp_key] = sub_path_logs
+            path_hofs[sp_key] = sub_path_hofs
 
-        return paths, path_logs, shortest_paths
+        return paths, path_logs, path_hofs, sps_dict
 
     def get_n_days(self, pop):
+        """
+        Get estimate of max travel time of inds in *pop* in whole days
+        """
         boat_speed = min(self.vessel.speeds)
         max_travel_time = 0
-        # Get conservative estimate of number of days of travel time
         for ind in pop:
-            # Initialize variables
             travel_time = 0.0
             for e in range(len(ind) - 1):
                 p1, p2 = sorted((ind[e][0], ind[e + 1][0]))
@@ -306,30 +325,30 @@ class RoutePlanner:
                 travel_time += e_travel_time
             if travel_time > max_travel_time:
                 max_travel_time = travel_time
-        n_days = int(math.ceil(max_travel_time / 24))
-        print('Number of days:', n_days)
-        return n_days
+        days = int(math.ceil(max_travel_time / 24))
+        print('Number of days:', days)
+        return days
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    # Gulf of Guinea, Gulf of Mexico
-    _start, _end = (3.14516, 4.68508), (-94.5968, 26.7012)
+    # # Gulf of Guinea, Gulf of Mexico
+    # _start, _end = (3.14516, 4.68508), (-94.5968, 26.7012)
     # South Atlantic (Brazil), Caribbean Sea
     # _start, _end = (-23.4166, -7.2574), (-72.3352, 12.8774)
-    # Laccadive Sea, Gulf of Aden
-    # _start, _end = (77.7962, 4.90087), (48.1425, 12.5489)
+    # Mediterranean Sea, Gulf of Aden
+    # _start, _end = (29.188952, 32.842985), (48.1425, 12.5489)
     # Normandy, Canada
     # _start, _end = (-5.352121, 48.021295), (-53.306878, 46.423969)
     # Normandy, Canada
-    # _start, _end = (34.252773, 43.461197), (53.131866, 13.521350)
+    # _start, _end = (-95, 10), (89, 9)
     # Gulf of Bothnia, Gulf of Mexico
     # _start, _end = (20.891193, 58.464147), (-85.063585, 29.175463)
     # North UK, South UK
-    # _start, _end = (3.891292, 60.088472), (-7.562237, 47.403357)
+    _start, _end = (3.891292, 60.088472), (-7.562237, 47.403357)
 
-    _route_planner = RoutePlanner(_start, _end, seca_factor=1.2, incl_curr=True)
-    _paths, _path_logs, _init_routes = _route_planner.nsga2(seed=1)
+    _route_planner = RoutePlanner(_start, _end, eca_f=1.2, incl_curr=True)
+    _paths, _path_logs, _path_hofs, _init_routes = _route_planner.nsga2(seed=1)
 
     # Save parameters
     timestamp = datetime.now()
@@ -354,7 +373,9 @@ if __name__ == "__main__":
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    # front = np.array([_ind.fitness.values for _ind in sub_path_pop])
-    # plt.scatter(front[:, 0], front[:, 1], c="b")
-    # plt.axis("tight")
-    # plt.show()
+    sub_path_pop = _paths[0][0]
+
+    front = np.array([_ind.fitness.values for _ind in sub_path_pop])
+    plt.scatter(front[:, 0], front[:, 1], c="b")
+    plt.axis("tight")
+    plt.show()
