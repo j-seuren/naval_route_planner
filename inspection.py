633d0d3d6cb076d083edd9f3885da0712d860f57
JobS <job.seuren@gmail.com>
2020-07-10 19:13:40 +0200

    Changed filename

--- inspection.py
+++ inspection.py
@@ -1,4 +1,5 @@
 import datetime
+import main
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,6 +8,7 @@ import pickle
 
 from matplotlib import cm
 from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def plot_stats(path_logs, name):
@@ -39,19 +41,25 @@ def plot_stats(path_logs, name):
 class RoutePlotter:
     def __init__(self,
                  paths,
-                 vessel,
-                 secas,
                  n_plots,
-                 id_dict,
-                 plot_secas=True,
+                 vessel=None,
+                 titles=None,
+                 ecas=None,
                  show_colorbar=True,
                  uin=None,
                  vin=None,
                  lons=None,
                  lats=None):
-        self.vessel = vessel
         self.col_map = cm.rainbow
-        self.ID_dict = id_dict
+        if titles:
+            self.titles = titles
+        else:
+            self.titles = ['no_title'] * n_plots
+
+        if vessel:
+            self.vessel = vessel
+        else:
+            self.vessel = main.Vessel()
 
         if uin is not None:
             self.uin = uin
@@ -93,7 +101,7 @@ class RoutePlotter:
             r, c = next(r_iter), next(c_iter)
             self.ax[i] = plt.subplot2grid((rows, columns), (r, c))
 
-            self.ax[i].set_title("{}".format(list(self.ID_dict.keys())[i]))
+            self.ax[i].set_title("{}".format(self.titles[i]))
             self.m[i] = Basemap(projection='merc', resolution='c',
                                 llcrnrlat=bot, urcrnrlat=top,
                                 llcrnrlon=lef, urcrnrlon=rig, ax=self.ax[i])
@@ -108,45 +116,55 @@ class RoutePlotter:
                 # Transform vector and coordinate data
                 vec_lon = int(uin.shape[1] / (2 * 360 / (rig - lef)))
                 vec_lat = int(uin.shape[0] / (2 * 180 / (top - bot)))
-                u_rot, v_rot, x, y = self.m[i].transform_vector(uin, vin, lons, lats, vec_lon, vec_lat, returnxy=True)
+                u_rot, v_rot, x, y = self.m[i].transform_vector(uin, vin,
+                                                                lons, lats,
+                                                                vec_lon, vec_lat,
+                                                                returnxy=True)
 
-                vec_plot = self.m[i].quiver(x, y, u_rot, v_rot, color='darkgray', scale=50, width=.002)
+                vec_plot = self.m[i].quiver(x, y, u_rot, v_rot,
+                                            color='gray', scale=50, width=.002)
                 plt.quiverkey(vec_plot, 0.2, -0.2, 1, '1 knot', labelpos='W')  # Position and reference label
 
-            if plot_secas:
-                for poly in secas:
-                    lon, lat = poly.exterior.xy
+            if ecas:
+                for eca in ecas:
+                    lon, lat = eca.exterior.xy
                     x, y = self.m[i](lon, lat)
                     self.m[i].plot(x, y, 'o-', markersize=2, linewidth=1)
 
             if show_colorbar:
                 # Create color bar
                 sm = plt.cm.ScalarMappable(cmap=self.col_map)
-                col_bar = plt.colorbar(sm, norm=plt.Normalize(vmin=min(vessel.speeds), vmax=max(vessel.speeds)))
-                max_s = max(vessel.speeds)
-                min_s = min(vessel.speeds)
-                col_bar.ax.set_yticklabels(['{}'.format(min_s),
-                                            '{}'.format(round((1 / 5) * (max_s - min_s) + min_s, 1)),
-                                            '{}'.format(round((2 / 5) * (max_s - min_s) + min_s, 1)),
-                                            '{}'.format(round((3 / 5) * (max_s - min_s) + min_s, 1)),
-                                            '{}'.format(round((4 / 5) * (max_s - min_s) + min_s, 1)),
-                                            '{}'.format(round(max_s, 1))])
+                divider = make_axes_locatable(self.ax[i])  # Width of cax is x% of ax, padding cax <-> ax: 0.05inch
+                cax = divider.append_axes("right", size="1%", pad=0.05)
+                col_bar = plt.colorbar(sm, norm=plt.Normalize(vmin=min(vessel.speeds), vmax=max(vessel.speeds)),
+                                       cax=cax)
+                min_s, max_s = min(vessel.speeds), max(vessel.speeds)
+                col_bar.ax.set_yticklabels(['%.1f' % round(min_s, 1),
+                                            '%.1f' % round((1 / 5) * (max_s - min_s) + min_s, 1),
+                                            '%.1f' % round((2 / 5) * (max_s - min_s) + min_s, 1),
+                                            '%.1f' % round((3 / 5) * (max_s - min_s) + min_s, 1),
+                                            '%.1f' % round((4 / 5) * (max_s - min_s) + min_s, 1),
+                                            '%.1f' % round(max_s, 1)])
 
-    def plot_global_routes(self, global_routes, i):
-        for pop in global_routes:
-            for ind in pop:
-                # Plot edges
-                waypoints = [item[0] for item in ind]
-                edges = zip(waypoints[:-1], waypoints[1:])
-                for e in edges:
-                    self.m[i].drawgreatcircle(e[0][0], e[0][1], e[1][0], e[1][1], linewidth=2, color='black', zorder=1)
+    def plot_shortest_paths(self, shortest_paths, i):
+        for shortest_path in shortest_paths.values():
+            for sub_shortest_path in shortest_path.values():
+                for ind in sub_shortest_path.values():
+                    # Plot edges
+                    waypoints = [item[0] for item in ind]
+                    edges = zip(waypoints[:-1], waypoints[1:])
+                    for e in edges:
+                        self.m[i].drawgreatcircle(e[0][0], e[0][1],
+                                                  e[1][0], e[1][1],
+                                                  linewidth=2, color='black', zorder=1)
 
     def plot_individuals(self, inds, i):
         for ind in inds:
             # Create colors
             true_speeds = [item[1] for item in ind[:-1]]
             normalized_speeds = [(speed - min(self.vessel.speeds)) /
-                                 (max(self.vessel.speeds) - min(self.vessel.speeds)) for speed in true_speeds] + [0]
+                                 (max(self.vessel.speeds) - min(self.vessel.speeds))
+                                 for speed in true_speeds] + [0]
 
             # Plot edges
             waypoints = [item[0] for item in ind]
@@ -159,44 +177,42 @@ class RoutePlotter:
 
 
 if __name__ == "__main__":
-    import main
-
-    route_planner = main.RoutePlanner(seca_factor=1.2,
+    route_planner = main.RoutePlanner(eca_f=1.2,
                                       res='c',
                                       spl_th=4,
                                       vessel_name='Fairmaster',
                                       incl_curr=True)
 
-    _ID_dict = {'1    current': '16_57_02',
-                '1 no current': '17_46_15',
-                '2 1,200    current': '',
-                '2 1,200 no current': '21_31_13'}
+    _ID_dict = {'eca test': '14_44_57'}
 
     # # Brazil -> Caribbean Sea
-    # _ID_dict = {'   Currents -    SECA': '',
-    #             '   Currents - no SECA': '',
-    #             'no Currents -    SECA': '11_40_38',
-    #             'no Currents - no SECA': '11_42_36'}
+    # _ID_dict = {'   Currents -    ECA': '',
+    #             '   Currents - no ECA': '',
+    #             'no Currents -    ECA': '11_40_38',
+    #             'no Currents - no ECA': '11_42_36'}
 
     # # Scandinavia -> Caribbean Sea
-    # _ID_dict = {'   Currents -    SECA': '16_55_20',
-    #             '   Currents - no SECA': '17_06_23',
-    #             'no Currents -    SECA': '17_10_02',
-    #             'no Currents - no SECA': '17_54_32'}
+    # _ID_dict = {'   Currents -    ECA': '16_55_20',
+    #             '   Currents - no ECA': '17_06_23',
+    #             'no Currents -    ECA': '17_10_02',
+    #             'no Currents - no ECA': '17_54_32'}
 
-    # _ID_dict = {'   Currents -    SECA': '15_13_57',  # Currents and seca
-    #             '   Currents - no SECA': '15_21_43',  # Currents and no seca
-    #             'no Currents -    SECA': '15_25_58',  # No currents and seca
-    #             'no Currents - no SECA': '15_24_44'}  # No currents and no seca
+    # _ID_dict = {'   Currents -    ECA': '15_13_57',  # Currents and eca
+    #             '   Currents - no ECA': '15_21_43',  # Currents and no eca
+    #             'no Currents -    ECA': '15_25_58',  # No currents and eca
+    #             'no Currents - no ECA': '15_24_44'}  # No currents and no eca
 
     with open('C:/dev/data/seca_areas_csv', 'rb') as file:
-        _secas = pickle.load(file)
+        _ecas = pickle.load(file)
 
     with open('output/paths/{}_paths'.format(next(iter(_ID_dict.values()))), 'rb') as f:
         first_paths = pickle.load(f)
 
-    _n_plots = len(_ID_dict)
-    plotter = RoutePlotter(first_paths, route_planner.vessel, _secas, _n_plots, _ID_dict)
+    plotter = RoutePlotter(paths=first_paths,
+                           n_plots=len(_ID_dict),
+                           vessel=route_planner.vessel,
+                           titles=list(_ID_dict.keys()),
+                           ecas=_ecas)
 
     for _i, key in enumerate(_ID_dict.keys()):
         with open('output/paths/{}_paths'.format(_ID_dict[key]), 'rb') as f:
@@ -228,13 +244,15 @@ if __name__ == "__main__":
                 best_inds['Minimal time: '] = tt_ind
 
         plotter.plot_individuals(best_inds.values(), _i)
-        plotter.plot_global_routes(_global_routes, _i)
+        plotter.plot_shortest_paths(_global_routes, _i)
 
         _n_days = math.ceil(max([ind.fitness.values[0] for ind in best_inds.values()]))
         print('n_days:', _n_days)
         _start_date = datetime.datetime(2016, 1, 1)
-        route_planner.evaluator.current_data = ocean_current.CurrentData(
-            _start_date, _n_days)
+
+        if route_planner.incl_curr:
+            route_planner.evaluator.current_data = ocean_current.CurrentOperator(
+                _start_date, _n_days)
 
         for k, _ind in best_inds.items():
             fit = route_planner.tb.evaluate(_ind)
