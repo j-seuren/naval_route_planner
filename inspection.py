c866f33d96c8d18833dc2169044eae140005c957
JobS <job.seuren@gmail.com>
2020-07-15 18:04:21 +0200

    Update inspection.py
    Passing bounds to RoutePlotter
    Introduced RoutePlotter.plot_routes()
    Introduced plot_fronts()
    Updated printing objective values

--- inspection.py
+++ inspection.py
@@ -40,7 +40,7 @@ def plot_stats(path_logs, name):
 
 class RoutePlotter:
     def __init__(self,
-                 paths,
+                 bounds,
                  n_plots,
                  vessel=None,
                  titles=None,
@@ -56,6 +56,10 @@ class RoutePlotter:
         else:
             self.titles = ['no_title'] * n_plots
 
+        self.ecas = ecas
+        self.show_colorbar = show_colorbar
+        self.n_plots = n_plots
+
         if vessel:
             self.vessel = vessel
         else:
@@ -66,201 +70,223 @@ class RoutePlotter:
             self.vin = vin
             self.lons = lons
             self.lats = lats
-            plot_currents = True
+            self.plot_currents = True
         else:
             self.uin = self.vin = self.lons = self.lats = None
-            plot_currents = False
+            self.plot_currents = False
 
         # Set extent
-        all_wps = [wp[0] for sp in paths.values()
-                   for pop in sp.values()
-                   for ind in pop
-                   for wp in ind]
-        minx = int(min(all_wps, key=lambda t: t[0])[0])
-        miny = int(min(all_wps, key=lambda t: t[1])[1])
-        maxx = int(max(all_wps, key=lambda t: t[0])[0])
-        maxy = int(max(all_wps, key=lambda t: t[1])[1])
+        minx, miny, maxx, maxy = bounds
         margin = 5
-        lef = max(minx - margin, -180)
-        rig = min(maxx + margin, 180)
-        bot = max(miny - margin, -90)
-        top = min(maxy + margin, 90)
+        self.lef = max(minx - margin, -180)
+        self.rig = min(maxx + margin, 180)
+        self.bot = max(miny - margin, -90)
+        self.top = min(maxy + margin, 90)
 
-        rows = round(math.sqrt(n_plots))
-        columns = math.ceil(math.sqrt(n_plots))
+        self.rows = int(round(math.sqrt(n_plots)))
+        self.columns = int(math.ceil(math.sqrt(n_plots)))
 
-        r_iter = iter([r for ro in range(rows) for r in [ro] * columns])
-        c_iter = iter(list(range(columns)) * rows)
+    def plot_routes(self, results_files, best_inds_files):
+        fig = plt.figure()
+        fig.suptitle('Routes')
 
-        plt.figure()
-
-        self.ax = [None] * n_plots
-        self.m = [None] * n_plots
-
-        for i in range(n_plots):
+        r_iter = iter([r for ro in range(self.rows) for r in [ro] * self.columns])
+        c_iter = iter(list(range(self.columns)) * self.rows)
+        for i, result in enumerate(results_files):
             r, c = next(r_iter), next(c_iter)
-            self.ax[i] = plt.subplot2grid((rows, columns), (r, c))
 
-            self.ax[i].set_title("{}".format(self.titles[i]))
-            self.m[i] = Basemap(projection='merc', resolution='c',
-                                llcrnrlat=bot, urcrnrlat=top,
-                                llcrnrlon=lef, urcrnrlon=rig, ax=self.ax[i])
-            self.m[i].drawparallels(np.arange(-90., 90., 10.),
-                                    labels=[1, 0, 0, 0], fontsize=10)
-            self.m[i].drawmeridians(np.arange(-180., 180., 10.),
-                                    labels=[0, 0, 0, 1], fontsize=10)
-            self.m[i].drawcoastlines()
-            self.m[i].fillcontinents()
+            shortest_paths = result['shortest_paths']
+            individuals = best_inds_files[i]
 
-            if plot_currents:
+            ax = plt.subplot2grid((self.rows, self.columns), (r, c))
+            ax.set_title("{}".format(self.titles[i]))
+            m = Basemap(projection='merc', resolution='c',
+                        llcrnrlat=self.bot, urcrnrlat=self.top,
+                        llcrnrlon=self.lef, urcrnrlon=self.rig,
+                        ax=ax)
+            m.drawparallels(np.arange(-90., 90., 10.),
+                            labels=[1, 0, 0, 0], fontsize=10)
+            m.drawmeridians(np.arange(-180., 180., 10.),
+                            labels=[0, 0, 0, 1], fontsize=10)
+            m.drawcoastlines()
+            m.fillcontinents()
+
+            # Plot initial routes
+            for p in shortest_paths.values():
+                for sp in p.values():
+                    for shortest_path in sp.values():
+                        waypoints = [item[0] for item in shortest_path]
+                        edges = zip(waypoints[:-1], waypoints[1:])
+                        for e in edges:
+                            m.drawgreatcircle(e[0][0], e[0][1], e[1][0], e[1][1],
+                                              linewidth=2, color='black', zorder=1)
+
+            # Plot individuals
+            for p in individuals.values():
+                for sp in p.values():
+                    for ind in sp:
+                        # Create colors
+                        true_speeds = [item[1] for item in ind[:-1]]
+                        normalized_speeds = [(speed - min(self.vessel.speeds)) / (
+                                max(self.vessel.speeds) - min(self.vessel.speeds)) for speed in true_speeds] + [0]
+
+                        # Plot edges
+                        waypoints = [item[0] for item in ind]
+                        edges = zip(waypoints[:-1], waypoints[1:])
+                        for j, e in enumerate(edges):
+                            m.drawgreatcircle(e[0][0], e[0][1], e[1][0], e[1][1], linewidth=2,
+                                              color=self.col_map(normalized_speeds[j]), zorder=1)
+                        for j, (x, y) in enumerate(waypoints):
+                            m.scatter(x, y, latlon=True, color='dimgray', marker='o', s=5, zorder=2)
+
+            if self.plot_currents:
                 # Transform vector and coordinate data
-                vec_lon = int(uin.shape[1] / (2 * 360 / (rig - lef)))
-                vec_lat = int(uin.shape[0] / (2 * 180 / (top - bot)))
-                u_rot, v_rot, x, y = self.m[i].transform_vector(uin, vin,
-                                                                lons, lats,
-                                                                vec_lon, vec_lat,
-                                                                returnxy=True)
+                vec_lon = int(self.uin.shape[1] / (2 * 360 / (self.rig - self.lef)))
+                vec_lat = int(self.uin.shape[0] / (2 * 180 / (self.top - self.bot)))
+                u_rot, v_rot, x, y = m.transform_vector(self.uin, self.vin, self.lons, self.lats, vec_lon, vec_lat,
+                                                        returnxy=True)
 
-                vec_plot = self.m[i].quiver(x, y, u_rot, v_rot,
-                                            color='gray', scale=50, width=.002)
+                vec_plot = m.quiver(x, y, u_rot, v_rot, color='gray', scale=50, width=.002)
                 plt.quiverkey(vec_plot, 0.2, -0.2, 1, '1 knot', labelpos='W')  # Position and reference label
 
-            if ecas:
-                for eca in ecas:
+            if self.ecas:
+                for eca in self.ecas:
                     lon, lat = eca.exterior.xy
-                    x, y = self.m[i](lon, lat)
-                    self.m[i].plot(x, y, 'o-', markersize=2, linewidth=1)
+                    x, y = m(lon, lat)
+                    m.plot(x, y, 'o-', markersize=2, linewidth=1)
 
-            if show_colorbar:
+            if self.show_colorbar:
                 # Create color bar
                 sm = plt.cm.ScalarMappable(cmap=self.col_map)
-                divider = make_axes_locatable(self.ax[i])  # Width of cax is x% of ax, padding cax <-> ax: 0.05inch
+                divider = make_axes_locatable(ax)
                 cax = divider.append_axes("right", size="1%", pad=0.05)
-                col_bar = plt.colorbar(sm, norm=plt.Normalize(vmin=min(vessel.speeds), vmax=max(vessel.speeds)),
+                col_bar = plt.colorbar(sm, norm=plt.Normalize(vmin=min(self.vessel.speeds),
+                                                              vmax=max(self.vessel.speeds)),
                                        cax=cax)
-                min_s, max_s = min(vessel.speeds), max(vessel.speeds)
-                col_bar.ax.set_yticklabels(['%.1f' % round(min_s, 1),
-                                            '%.1f' % round((1 / 5) * (max_s - min_s) + min_s, 1),
-                                            '%.1f' % round((2 / 5) * (max_s - min_s) + min_s, 1),
-                                            '%.1f' % round((3 / 5) * (max_s - min_s) + min_s, 1),
-                                            '%.1f' % round((4 / 5) * (max_s - min_s) + min_s, 1),
-                                            '%.1f' % round(max_s, 1)])
+                min_s, max_s = min(self.vessel.speeds), max(self.vessel.speeds)
+                col_bar.ax.set_yticklabels(
+                    ['%.1f' % round(min_s, 1), '%.1f' % round((1 / 5) * (max_s - min_s) + min_s, 1),
+                     '%.1f' % round((2 / 5) * (max_s - min_s) + min_s, 1),
+                     '%.1f' % round((3 / 5) * (max_s - min_s) + min_s, 1),
+                     '%.1f' % round((4 / 5) * (max_s - min_s) + min_s, 1), '%.1f' % round(max_s, 1)])
 
-    def plot_shortest_paths(self, shortest_paths, i):
-        for shortest_path in shortest_paths.values():
-            for sub_shortest_path in shortest_path.values():
-                for ind in sub_shortest_path.values():
-                    # Plot edges
-                    waypoints = [item[0] for item in ind]
-                    edges = zip(waypoints[:-1], waypoints[1:])
-                    for e in edges:
-                        self.m[i].drawgreatcircle(e[0][0], e[0][1],
-                                                  e[1][0], e[1][1],
-                                                  linewidth=2, color='black', zorder=1)
 
-    def plot_individuals(self, inds, i):
-        for ind in inds:
-            # Create colors
-            true_speeds = [item[1] for item in ind[:-1]]
-            normalized_speeds = [(speed - min(self.vessel.speeds)) /
-                                 (max(self.vessel.speeds) - min(self.vessel.speeds))
-                                 for speed in true_speeds] + [0]
+def plot_fronts(front):
+    # Plot fronts
+    front = np.array([_ind.fitness.values for _ind in front])
 
-            # Plot edges
-            waypoints = [item[0] for item in ind]
-            edges = zip(waypoints[:-1], waypoints[1:])
-            for j, e in enumerate(edges):
-                self.m[i].drawgreatcircle(e[0][0], e[0][1], e[1][0], e[1][1], linewidth=2,
-                                          color=self.col_map(normalized_speeds[j]), zorder=1)
-            for j, (x, y) in enumerate(waypoints):
-                self.m[i].scatter(x, y, latlon=True, color='dimgray', marker='o', s=5, zorder=2)
+    ax = plt.subplot2grid((max_sp, n_p), (sp_key, p_key))
+    ax.scatter(front[:, 0], front[:, 1], c="b", s=1)
+    ax.set_title('P{} - SP{}'.format(p_key, sp_key))
+    ax.axis("tight")
+    ax.grid()
+    ax.set_xlabel('Travel time (days)')
+    ax.set_ylabel('Fuel costs (1000 USD per tonne)')
 
 
 if __name__ == "__main__":
-    route_planner = main.RoutePlanner(eca_f=1.2,
-                                      res='c',
-                                      spl_th=4,
-                                      vessel_name='Fairmaster',
-                                      incl_curr=True)
-
-    _ID_dict = {'hof test': '19_44_34'}
-
-    # # Brazil -> Caribbean Sea
-    # _ID_dict = {'   Currents -    ECA': '',
-    #             '   Currents - no ECA': '',
-    #             'no Currents -    ECA': '11_40_38',
-    #             'no Currents - no ECA': '11_42_36'}
-
-    # # Scandinavia -> Caribbean Sea
-    # _ID_dict = {'   Currents -    ECA': '16_55_20',
-    #             '   Currents - no ECA': '17_06_23',
-    #             'no Currents -    ECA': '17_10_02',
-    #             'no Currents - no ECA': '17_54_32'}
-
-    # _ID_dict = {'   Currents -    ECA': '15_13_57',  # Currents and eca
-    #             '   Currents - no ECA': '15_21_43',  # Currents and no eca
-    #             'no Currents -    ECA': '15_25_58',  # No currents and eca
-    #             'no Currents - no ECA': '15_24_44'}  # No currents and no eca
+    _ID_dict = {'spea2': '13_35_43',
+                'nsga2': '13_35_12',
+                'm-paes': '17_49_42'}
+    incl_curr = False
+    _start_date = datetime.datetime(2016, 1, 1)
+    planner = main.RoutePlanner(incl_curr=incl_curr)
 
     with open('C:/dev/data/seca_areas_csv', 'rb') as file:
         _ecas = pickle.load(file)
 
-    with open('output/paths/{}_paths'.format(next(iter(_ID_dict.values()))), 'rb') as f:
-        first_paths = pickle.load(f)
+    # Get outer bounds of all paths
+    _minx, _miny, _maxx, _maxy = 180, 90, -180, -90
+    for file_id in _ID_dict.values():
+        with open('output/result/{}'.format(file_id), 'rb') as f:
+            _result = pickle.load(f)
 
-    plotter = RoutePlotter(paths=first_paths,
+        for p_key in _result['fronts'].values():
+            for _front in p_key.values():
+                # Get outer bounds of paths
+                wps = np.asarray([row[0] for ind in _front for row in ind])
+                minx_i, miny_i = np.amin(wps, axis=0)
+                maxx_i, maxy_i = np.amax(wps, axis=0)
+                _minx, _miny = min(minx_i, _minx), min(miny_i, _miny)
+                _maxx, _maxy = max(maxx_i, _maxx), max(maxy_i, _maxy)
+
+    _bounds = (_minx, _miny, _maxx, _maxy)
+
+    plotter = RoutePlotter(bounds=_bounds,
                            n_plots=len(_ID_dict),
-                           vessel=route_planner.vessel,
                            titles=list(_ID_dict.keys()),
-                           ecas=_ecas)
+                           ecas=_ecas,
+                           vessel=planner.vessel)
 
-    for _i, key in enumerate(_ID_dict.keys()):
-        with open('output/paths/{}_paths'.format(_ID_dict[key]), 'rb') as f:
-            _paths = pickle.load(f)
+    all_results, all_best_inds = [], []
+    # Inspect best individuals for every ID dict key
+    for f_idx, f_title in enumerate(_ID_dict.keys()):
+        with open('output/result/{}'.format(_ID_dict[f_title]), 'rb') as f:
+            _result = pickle.load(f)
 
-        with open('output/logs/{}_logs'.format(_ID_dict[key]), 'rb') as f:
-            _path_logs = pickle.load(f)
+        # Initialize pareto_fig
+        pareto_fig, axs = plt.subplots(squeeze=False)
+        pareto_fig.suptitle(f_title)
 
-        with open('output/hofs/{}_hofs'.format(_ID_dict[key]), 'rb') as f:
-            _hofs = pickle.load(f)
+        n_p = len(_result['fronts'])
+        max_sp = max([len(_result['fronts'][p]) for p in _result['fronts']])
 
-        with open('output/glob_routes/{}_init_routes'.format(_ID_dict[key]), 'rb') as f:
-            _global_routes = pickle.load(f)
-
-        plot_stats(_path_logs, key)
-
+        # Get best individuals in front
         best_inds = {}
-        for _path in _hofs.values():
-            for _hof in _path.values():
-                min_tt, min_fc = math.inf, math.inf
-                tt_ind, fc_ind = None, None
-                for i, _fit in enumerate(_hof.keys):
-                    tt, fc = _fit.values
-                    if tt < min_tt:
-                        tt_ind = _hof.items[i]
-                        min_tt = tt
-                    if fc < min_fc:
-                        fc_ind = _hof.items[i]
-                        min_fc = fc
+        max_days = 0
+        for p_key, p_val in _result['fronts'].items():
+            best_inds[p_key] = {}
+            for sp_key, _front in p_val.items():
+                # Get min time, min fuel and min fitness individuals
+                fit_values = np.asarray([_ind.fitness.values for _ind in _front])
 
-                best_inds['Minimal fuel: '] = fc_ind
-                best_inds['Minimal time: '] = tt_ind
+                time_ind = _front[np.argmin(fit_values[:, 0])]
+                fuel_ind = _front[np.argmin(fit_values[:, 1])]
+                fit_ind = _front[0]
 
-        plotter.plot_individuals(best_inds.values(), _i)
-        plotter.plot_shortest_paths(_global_routes, _i)
+                # Append best individuals to list
+                if sp_key == 0:
+                    best_inds[p_key]['fuel'] = [fuel_ind]
+                    best_inds[p_key]['time'] = [time_ind]
+                    best_inds[p_key]['fit'] = [fit_ind]
+                else:
+                    best_inds[p_key]['fuel'].append(fuel_ind)
+                    best_inds[p_key]['time'].append(time_ind)
+                    best_inds[p_key]['fit'].append(fit_ind)
 
-        _n_days = math.ceil(max([ind.fitness.values[0] for ind in best_inds.values()]))
-        print('n_days:', _n_days)
-        _start_date = datetime.datetime(2016, 1, 1)
+                # Get max days
+                max_days = math.ceil(max(max_days, max([fuel_ind.fitness.values[0],
+                                                        time_ind.fitness.values[0],
+                                                        fit_ind.fitness.values[0]])
+                                         )
+                                     )
 
-        if route_planner.incl_curr:
-            route_planner.evaluator.current_data = ocean_current.CurrentOperator(
-                _start_date, _n_days)
+                # Plot fronts
+                plot_fronts(_front)
 
-        for k, _ind in best_inds.items():
-            fit = route_planner.tb.evaluate(_ind)
-            print('{0:>20}, {1}'.format(list(_ID_dict.keys())[_i], k), fit)
-            print('{0:>20}, {1} ORIGINAL'.format(list(_ID_dict.keys())[_i], k), _ind.fitness.values)
-            # print('DIFF ORIGINAL: {}'.format(np.subtract(_ind.fitness.values, fit)))
+        print('n_days:', max_days)
+
+        if incl_curr:
+            planner.evaluator.current_data = ocean_current.CurrentOperator(
+                _start_date, max_days)
+
+        for p_key in best_inds:
+            for _k, _ind_list in best_inds[p_key].items():
+                total_fit = total_original_fit = np.zeros(2)
+                for _ind in _ind_list:
+                    fit = np.array(planner.tb.evaluate(_ind))
+                    original_fit = np.array(_ind.fitness.values)
+                    total_fit += fit
+                    total_original_fit += original_fit
+
+                file_key = list(_ID_dict.keys())[f_idx]
+                print('File {} Path {} Obj {:>4} real'.format(file_key, p_key, _k), np.round(total_fit, 2))
+                # print('File {} Path {} Obj {:>4} orig'.format(file_key, _p, _k), np.round(total_original_fit, 2))
+                # print('DIFF ORIGINAL: {}'.format(np.subtract(_ind.fitness.values, fit)))
+
+        all_results.append(_result)
+        all_best_inds.append(best_inds)
+
+    plotter.plot_routes(all_results, all_best_inds)
 
     plt.show()
