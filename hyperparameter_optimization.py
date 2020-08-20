17747c9340f9a724f124e88da649c56af04704eb
JobS <job.seuren@gmail.com>
2020-08-20 12:39:09 +0200

    Updated hyper parameter tuning

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import skopt
@@ -5,83 +6,84 @@ import time
 
 from datetime import datetime
 from main import RoutePlanner
+from skopt import plots, forest_minimize
 
-INPUT = {'start': (78, 5),
-         'end': (49, 12),
-         'start_date': datetime(2016, 1, 1),
-         'vessel_name': 'Fairmaster',
-         'current': False,
-         'eca_f': 1.05,
-         'vlsfo_price': 0.3}
+for gauss in [True, False]:
+    PARS = {'gauss': gauss}
+    CALL = 0
+    planner = RoutePlanner()
 
 
-def train_evaluate(search_params):
-    print(search_params)
-    start_func_time = time.time()
+    def train_evaluate(search_params):
+        global CALL
+        start_func_time = time.time()
 
-    params = {'res': 'c',
-              'spl_th': 4,
-              'shape': 3,
-              'scale_factor': 0.1,
-              'recomb': 5,
-              'l_fails': 5,
-              'l_moves': 10,
-              'n_bar': 50,
-              **search_params}
+        start_ends = [((20.89, 58.46), (-85.06, 29.18))]  # Gulf of Bothnia, Gulf of Mexico
+        parameters = {**PARS, **search_params}
+        print(parameters)
 
-    start = INPUT['start']
-    end = INPUT['end']
-    start_date = INPUT['start_date']
-    vessel_name = INPUT['vessel_name']
-    incl_curr = INPUT['current']
-    eca_f = INPUT['eca_f']
-    vlsfo_price = INPUT['vlsfo_price']
+        planner.update_parameters(parameters)
+        result = planner.compute(start_ends, seed=None)[0]
 
-    route_planner = RoutePlanner(start, end, start_date, vessel_name,
-                                 incl_curr, eca_f, vlsfo_price, params)
+        end_func_time = time.time() - start_func_time
+        path_fits = []
+        for path in result['fronts'].values():
+            path_fitness = np.empty(2)
+            for sub_path in path.values():
+                path_fitness += sub_path[0].fitness.values
+            path_fits.append(path_fitness)
+        avg_fit = np.mean(path_fits, axis=0)
+        score_arr = np.append(avg_fit, end_func_time)
+        weights = np.array([200, 100, 1])
+        score = np.dot(score_arr, weights)
 
-    result = route_planner.nsgaii.compute(seed=1)
+        CALL += 1
+        print('call:', CALL)
 
-    end_func_time = time.time() - start_func_time
-    path_fits = []
-    for path in result['fronts'].values():
-        path_fitness = np.empty(2)
-        for sub_path in path.values():
-            path_fitness += sub_path[0].fitness.values
-        path_fits.append(path_fitness)
-    avg_fit = np.mean(path_fits, axis=0)
-    score_arr = np.append(avg_fit, end_func_time)
-    weights = np.array([200, 100, 1])
-    score = np.dot(score_arr, weights)
-
-    return score
+        return score
 
 
-SPACE = [skopt.space.Integer(10, 2000, name='gen', prior='uniform'),
-         skopt.space.Integer(8, 1000, name='n', prior='log-uniform'),
-         skopt.space.Real(0.5, 1.0, name='cxpb', prior='uniform'),
-         skopt.space.Real(0.5, 1.0, name='mutpb', prior='uniform'),
-         # skopt.space.Integer(5, 500, name='n_bar', prior='log-uniform'),
-         # skopt.space.Integer(1, 10, name='recomb', prior='uniform'),
-         # skopt.space.Integer(1, 20, name='l_fails', prior='uniform'),
-         # skopt.space.Integer(1, 20, name='l_moves', prior='uniform'),
-         skopt.space.Real(0.1, 5.0, name='width_ratio', prior='uniform'),
-         skopt.space.Real(0.1, 5.0, name='radius', prior='uniform'),
-         skopt.space.Real(0.1, 5.0, name='del_factor', prior='uniform')]
+    SPACE = [
+        # skopt.space.Integer(10, 500, name='gen'),
+        # skopt.space.Categorical([4 * i for i in range(1, 151)], name='n'),
+        # skopt.space.Real(0.5, 1.0, name='cxpb'),
+        # skopt.space.Real(0.5, 1.0, name='mutpb'),
+        # skopt.space.Integer(5, 500, name='nBar', prior='log-uniform'),
 
 
-@skopt.utils.use_named_args(SPACE)
-def objective(**params):
-    return train_evaluate(params)
+        # skopt.space.Integer(1, 10, name='recomb'),
+        # skopt.space.Integer(1, 20, name='fails'),
+        # skopt.space.Integer(1, 20, name='moves'),
+        skopt.space.Real(0.1, 10.0, name='widthRatio'),
+        skopt.space.Real(0.1, 10.0, name='radius', prior='log-uniform'),
+        # skopt.space.Real(0.1, 5.0, name='delFactor')
+    ]
 
 
-results = skopt.forest_minimize(objective, SPACE, n_calls=500, n_random_starts=10)
+    @skopt.utils.use_named_args(SPACE)
+    def objective(**params):
+        return train_evaluate(params)
 
-with open('tuned_parameters_result', 'wb') as fh:
-    pickle.dump(results, fh)
 
-best_params = {k.name: results.x[i] for i, k in enumerate(SPACE)}
+    n_calls = 250
+    results = forest_minimize(objective, SPACE, n_calls=n_calls, n_random_starts=10)
 
-print('best result: ', results.fun)
-print('best parameters: ', best_params)
+    timestamp = datetime.now()
+    timestamp = '{0:%H_%M_%S}'.format(timestamp)
 
+    fp = 'output/tuning/{}_{}_calls_tuned_parameters_result'.format(timestamp, n_calls)
+    with open(fp, 'wb') as fh:
+        pickle.dump(results, fh)
+        print('Saved to: ', fp)
+
+    best_params = {k.name: results.x[i] for i, k in enumerate(SPACE)}
+
+    print('best result: ', results.fun)
+    print('best parameters: ', best_params)
+
+    ev_ax = plots.plot_evaluations(results)
+    plt.savefig('output/tuning/{}_{}_calls_tune_eval.png'.format(timestamp, n_calls))
+    obj_ax = plots.plot_objective(results)
+    plt.savefig('output/tuning/{}_{}_calls_tune_obj.png'.format(timestamp, n_calls))
+
+plt.show()
