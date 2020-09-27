3d7b1b237299891077dc947ca3c8bcbc5967956f
Job Seuren <JobS@Ortec.Com>
2020-09-27 10:25:46 +0200

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -3,6 +3,13 @@ from scipy.spatial.distance import cdist
 from deap.tools.indicator import hv
 
 
+def hypervolume(front):
+    wobj = np.array([ind.fitness.wvalues for ind in front]) * -1
+    ref = np.max(wobj, axis=0) + 1
+
+    return hv.hypervolume(wobj, ref)
+
+
 def generational_distance(pop, ref):
     if len(pop) > 0:
         refFits = np.array([ind.fitness.values for ind in ref])
@@ -11,11 +18,3 @@ def generational_distance(pop, ref):
         return np.average(np.min(dist, axis=0))
     else:
         return 1e+6
-
-
-def hypervolume(front):
-    wobj = np.array([ind.fitness.wvalues for ind in front]) * -1
-    ref = np.max(wobj, axis=0) + 1
-
-    return hv.hypervolume(wobj, ref)
-
